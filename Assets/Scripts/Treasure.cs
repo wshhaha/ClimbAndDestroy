5a409f550fa5446e394b2a441292b027b02be178
wshhaha <solvergin@gmail.com>
2018-12-10 21:09:23 +0900

    1

--- Assets/Scripts/Treasure.cs
+++ Assets/Scripts/Treasure.cs
@@ -12,6 +12,25 @@ public class Treasure : MonoBehaviour
     public void Openbox()
     {        
         GetComponentInChildren<UIButton>().enabled = false;
+        Rewardt();
         rewards.SetActive(true);
+    }
+    void Rewardt()
+    {
+        int rant = Random.Range(0, 100);
+        if (rant < 110)
+        {
+            int lotto = Random.Range(0, 100);
+            int num = 0;
+            if (lotto >= 0 && lotto < 80)
+            {
+                num = Random.Range(0, 2);
+            }
+            if (lotto >= 80 && lotto < 100)
+            {
+                num = Random.Range(2, 4);
+            }
+            rewards.GetComponent<Rewards>().Addreward("treasure", num);
+        }
     }
 }
